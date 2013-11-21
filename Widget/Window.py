2c882730157c2b84bc971071bfd6af6d92be51f2
Julia <juliabeike@yahoo.de>
2013-11-21 16:19:50 +0100

    +Code verkleinert,abhängikeit auf Responsetyp Signed-off-by: Julia <juliabeike@yahoo.de>

--- Widget/Window.py
+++ Widget/Window.py
@@ -5,7 +5,7 @@ MAINWINDOW
 
 @author: beike
 """
-import Design_Method,ResponseType,FilterOrder,Frequency_Specification
+import Design_Method,ResponseType,FilterOrder,Frequency_Specification,Unit_Box
 import sys
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import SIGNAL
@@ -15,15 +15,39 @@ class Window(QtGui.QWidget):
     def __init__(self):
         super(Window, self).__init__()        
         self.initUI()
-        
+        self.chose_design_list=(['Least-squares','LP',['Fs','Fpass','Fstop'],[48000,9600,12000],False,True],
+                               ['Least-squares','HP',['Fs','Fstop','Fpass'],[48000,9600,12000],False,True],
+                               ['Least-squares','BP',['Fs','Fstop1','Fpass1','Fstop2','Fpass2'],[48000,7200,9600,12000,14400],False,True],
+                               ['Least-squares','BS',['Fs','Fpass1','Fstop1','Fpass2','Fstop2'],[48000,7200,9600,12000,14400],False,True],
+                               ['Equiripple','LP',['Fs','Fpass','Fstop'],[48000,9600,12000],True,True],
+                               ['Equiripple','HP',['Fs','Fstop','Fpass'],[48000,9600,12000],True,True],
+                               ['Equiripple','BP',['Fs','Fstop1','Fpass1','Fstop2','Fpass2'],[48000,7200,9600,12000,14400],True,True],
+                               ['Equiripple','BS',['Fs','Fpass1','Fstop1','Fpass2','Fstop2'],[48000,7200,9600,12000,14400],True,True],      
+                               ['Window','LP',['Fs','Fc'],[48000,10800],False,True],
+                               ['Window','HP',['Fs','Fc'],[48000,10800],False,True],
+                               ['Window','BP',['Fs','Fc1','Fc2'],[48000,8400,13200],False,True],
+                               ['Window','BS',['Fs','Fc1','Fc2'],[48000,8400,13200],False,True], 
+                               ['Butterworth','LP',['Fs','Fc'],[48000,10800],True,True],
+                               ['Butterworth','HP',['Fs','Fc'],[48000,10800],True,True],
+                               ['Butterworth','BP',['Fs','Fc1','Fc2'],[48000,8400,13200],True,True],
+                               ['Butterworth','BS',['Fs','Fc1','Fc2'],[48000,8400,13200],True,True],
+                               ['Elliptic','LP',['Fs','Fpass'],[48000,9600],True,True],
+                               ['Elliptic','HP',['Fs','Fpass'],[48000,14400],True,True],
+                               ['Elliptic','BP',['Fs','Fpass1','Fpass2'],[48000,9600,12000],True,True],
+                               ['Elliptic','BS',['Fs','Fpass1','Fpass2'],[48000,9600,12000],True,True],
+                               ['Chebyshev','LP',['Fs','Fpass'],[48000,9600],True,True],
+                               ['Chebyshev','HP',['Fs','Fpass'],[48000,14400],True,True],
+                               ['Chebyshev','BP',['Fs','Fpass1','Fpass2'],[48000,9600,12000],True,True],
+                               ['Chebyshev','BS',['Fs','Fpass1','Fpass2'],[48000,9600,12000],True,True],
+        )
         
     def initUI(self): 
    
         self.rs=ResponseType.ResponseType()
         self.dm=Design_Method.Design_Method()
         self.fo=FilterOrder.FilterOrder()
-        self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fpass','Fstop'],[48000,9600,12000])
-        
+        self.fs=Unit_Box.Unit_Box(["Hz","Normalize 0 to 1","kHz","MHz","GHz"],['Fs','Fpass','Fstop'],[48000,9600,12000])
+        self.ms=Unit_Box.Unit_Box(["dB","Linear"],['Apass','Astop'],[1,80])
         """
         LAYOUT      
         """
@@ -48,48 +72,33 @@ class Window(QtGui.QWidget):
         """
         verbindung zwischen Design-Methode und Frequenz
         """
+        print "-----------------------------------------"
+        resp_type=self.rs.get_selected_button()
         if self.dm.radio_FIR.isChecked()==True: 
-            name=self.dm.combo_FilterMethod_FIR.currentText()
+            filtname=self.dm.combo_FilterMethod_FIR.currentText()
         else:
-            name=self.dm.combo_FilterMethod_IIR.currentText()
-        print name
-        if name=="Least-squares":
-            self.fs.close()
-            self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fpass','Fstop'],[48000,9600,12000])
-            self.layout.addWidget(self.fs,3,0)
-            self.fo.chekMinimal.setEnabled(False)
-            self.fo.chekManual.setChecked(True)
-        if name=="Equiripple":
-            self.fs.close()
-            self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fpass','Fstop'],[48000,9600,12000])
-            self.layout.addWidget(self.fs,3,0)
-            self.fo.chekMinimal.setEnabled(True)
-            self.fo.chekManual.setChecked(True)
-        if name=="Window":
-            self.fs.close()
-            self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fc'],[48000,10800])
-            self.layout.addWidget(self.fs,3,0)
-            self.fo.chekMinimal.setEnabled(False)
-            self.fo.chekManual.setChecked(True)
-        if name=="Butterworth":
-            self.fs.close()
-            self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fc'],[48000,10800])
-            self.layout.addWidget(self.fs,3,0)
-            self.fo.chekMinimal.setEnabled(True)
-            self.fo.chekManual.setChecked(True)
-        if name=='Elliptic':
-            self.fs.close()
-            self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fpass'],[48000,9600])
-            self.layout.addWidget(self.fs,3,0)
-            self.fo.chekMinimal.setEnabled(True)
-            self.fo.chekManual.setChecked(True)
-        if name=="Chebyshev":
-            self.fs.close()
-            self.fs=Frequency_Specification.Frequency_Specification(['Fs','Fpass'],[48000,9600])
-            self.layout.addWidget(self.fs,3,0)
-            self.fo.chekMinimal.setEnabled(True)
-            self.fo.chekManual.setChecked(True)
-    
+            filtname=self.dm.combo_FilterMethod_IIR.currentText()
+        print filtname + resp_type
+        j=0
+        i=0
+        while i==0:
+            print self.chose_design_list[j][0]+":"+self.chose_design_list[j][1]
+            if self.chose_design_list[j][0]==filtname and self.chose_design_list[j][1]==resp_type:
+                i=1
+                chosen=self.chose_design_list[j][2:]
+                print chosen
+            j=j+1
+        print "-----------------------------------------"   
+        self.rebild_window(chosen[0],chosen[1],chosen[2],chosen[3])
+        
+    def rebild_window(self,liste=[],default=[],enMin=True,chekMan=True):
+        
+
+        self.fs.Load_txt(liste,default)
+
+        self.fo.chekMinimal.setEnabled(enMin)
+        self.fo.chekManual.setChecked(chekMan)
+        
     
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
